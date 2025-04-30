83cc7aec67954d074f5bf6e3c0ce75f0f78986a4
s3r3 <faridmu851@gmail.com>
2025-05-01 00:29:50 +0700

    h

--- app/ui/register/register-form.tsx
+++ app/ui/register/register-form.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Button } from "components/ui/button";
+import { Checkbox } from "components/ui/checkbox";
+import { Input } from "components/ui/input";
+import { Label } from "components/ui/label";
 import { Icon } from "@iconify/react";
 
 export default function RegisterForm() {
